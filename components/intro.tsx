ea3dff3c7d33adb26cd56e59a1670d80b45e43dc
emindzanko1 <edzanko1@etf.unsa.ba>
2023-10-23 12:55:12 +0200

    Intro section completed

--- components/intro.tsx
+++ components/intro.tsx
@@ -59,34 +59,54 @@ export default function Intro() {
         experience. I enjoy building <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
       </motion.p>
-      <div className='flex flex-col sm:flex-row'>
+      <motion.div
+        className='flex flex-col sm:flex-row
+      items-center justify-center gap-2 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
         <Link
           href='#contact'
-          className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2
-        rounded-full'
+          className='group bg-gray-900 text-white px-7 py-3 flex items-center gap-2
+        rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105
+        transition'
         >
           Contact me here
-          <BsArrowRight />
+          <BsArrowRight className='opacity-70 group-hover:translate-x-1 transition' />
         </Link>
+
         <a
-          className='bg-white px-7 py-3 flex items-center gap-2
-        rounded-full'
+          className='group bg-white px-7 py-3 flex items-center gap-2
+        rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105
+        transition cursor-pointer border border-black/10'
+          href='/Emin_Džanko_CV.pdf'
+          download
         >
-          Download CV <HiDownload />
+          Download CV <HiDownload className='opacity-60 group-hover:-y-1 transition' />
         </a>
+
         <a
           className='bg-white p-4 text-gray-700 flex items-center gap-2
-        rounded-full'
+        rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105
+        transition cursor-pointer border border-black/10'
+        href='https://www.linkedin.com/in/emin-d%C5%BEanko-215772280/' target='_blank'
         >
           <BsLinkedin />
         </a>
+
         <a
           className='bg-white p-4 text-gray-700 flex items-center gap-2
-        text-[1.35rem] rounded-full'
+        text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105
+        transition cursor-pointer border border-black/10'
+        href='https://github.com/emindzanko1' target='_blank'
+
         >
           <FaGithubSquare />
         </a>
-      </div>
+      </motion.div>
     </section>
   );
 }
