b3f98b2833537109e164015f6907b4cbc0870251
emindzanko1 <edzanko1@etf.unsa.ba>
2023-10-24 17:20:30 +0200

    Fixed problem with switching links

--- components/about.tsx
+++ components/about.tsx
@@ -9,14 +9,14 @@ import { useActiveSectionContext } from '@/context/active-section-context';
 export default function About() {
   const { ref, inView } = useInView({
     threshold: 0.75,
-  });  
-  const { setActiveSection } = useActiveSectionContext();
+  });
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
-    if (inView) {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
       setActiveSection('About');
     }
-  }, [inView, setActiveSection]);
+  }, [inView, setActiveSection, timeOfLastClick]);
 
   return (
     <motion.section
@@ -30,20 +30,25 @@ export default function About() {
     >
       <SectionHeading>About Me</SectionHeading>
       <p className='mb-3'>
-        After graduating with a degree in <span className='font-medium'>Accounting</span>, I decided to pursue my
-        passion for programming. I enrolled in a coding bootcamp and learned{' '}
-        <span className='font-medium'>full-stack web development</span>.{' '}
-        <span className='italic'>My favorite part of programming</span> is the problem-solving aspect. I{' '}
-        <span className='underline'>love</span> the feeling of finally figuring out a solution to a problem. My core
-        stack is <span className='font-medium'>React, Next.js, Node.js, and MongoDB</span>. I am also familiar with
-        TypeScript and Prisma. I am always looking to learn new technologies. I am currently looking for a{' '}
-        <span className='font-medium'>full-time position</span> as a software developer.
+        As a final year student of <span className='font-medium'>Computer Science and Informatics</span>, set to
+        graduate by fall, I am looking for an opportunity to apply knowledge and skills gained throughout studies in a
+        professional environment.
+        <span className='italic'>Throughout my academic experience and independent work</span> I have had the
+        opportunity to work on various projects that allowed me to gain an understanding of{' '}
+        <span className='underline'> full-stack</span> application development. I have implemented{' '}
+        <span className='underline'>web applications</span>
+        {', '}
+        <span className='underline'>mobile applications</span>, and applications based on <span className='underline'>embedded systems</span>. I have gone
+        through various stages of working with applications, from design to implementation, testing, and maintenance. My
+        core stack is <span className='font-medium'>React, Next.js, Node.js, and MongoDB</span>. I am also familiar with
+        Java, MySQL, TypeScript, C, C++, .Net, and Kotlin. I am always looking to learn new technologies. I am currently
+        looking for a <span className='font-medium'>full-time position</span> as a software developer.
       </p>
 
       <p>
-        <span className='italic'>When I'm not coding</span>, I enjoy playing video games, watching movies, and playing
-        with my dog. I also enjoy <span className='font-medium'>learning new things</span>. I am currently learning
-        about <span className='font-medium'>history and philosophy</span>. I'm also learning how to play the guitar.
+        <span className='italic'>When I'm not coding</span>, I enjoy playing football, tennis or traveling. I also love{' '}
+        <span className='font-medium'>learning new languages</span>. I am currently learning about{' '}
+        <span className='font-medium'>Spanish language.</span>
       </p>
     </motion.section>
   );
--- components/intro.tsx
+++ components/intro.tsx
@@ -14,13 +14,14 @@ export default function Intro() {
   const { ref, inView } = useInView({
     threshold: 0.5,
   });
-  const { setActiveSection } = useActiveSectionContext();
+  const { setActiveSection, timeOfLastClick } = useActiveSectionContext();
 
   useEffect(() => {
-    if (inView) {
+    if (inView && Date.now() - timeOfLastClick > 1000) {
       setActiveSection('Home');
     }
-  }, [inView, setActiveSection]);
+  }, [inView, setActiveSection, timeOfLastClick]);
+
   return (
     <section id='home' className='mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]'>
       <div className='flex items-center justify-center'>
@@ -67,9 +68,10 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className='font-bold'>Hello, I'm Emin.</span> I'm a{' '}
-        <span className='font-bold'>full-stack developer</span> with <span className='font-bold'>8 years</span> of
-        experience. I enjoy building <span className='italic'>sites & apps</span>. My focus is{' '}
+        <span className='font-bold'>Hello, I'm Emin.</span> I'm a <span className='font-bold'>third-year student</span>{' '}
+        at the <span className='font-bold'>Faculty of Electrical Engineering in Sarajevo, </span> majoring in 
+        <span className='font-bold'> Computer Science and Informatics</span> . I enjoy
+        building <span className='italic'>sites & apps</span>. My focus is{' '}
         <span className='underline'>React (Next.js)</span>.
       </motion.h1>
       <motion.div
