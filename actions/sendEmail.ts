0cc8af83401ce4abfdc2f63df421b50aafebd753
emindzanko1 <edzanko1@etf.unsa.ba>
2023-10-25 12:40:55 +0200

    Completed contact section without useForm

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,31 +1,44 @@
 'use server';
 
+import React from 'react';
 import { Resend } from 'resend';
-import { validateString } from '@/lib/utils';
+import { validateString, getErrorMessage } from '@/lib/utils';
+import ContactFormEmail from '@/email/contact-form-email';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export const sendEmail = async (formData: FormData) => {
+export const senderEmail = async (formData: FormData) => {
   const senderEmail = formData.get('senderEmail');
   const message = formData.get('message');
 
-  if(!validateString(senderEmail, 500)) {
+  if (!validateString(senderEmail, 500)) {
     return {
-        error: 'Invalid sender email',
-    }
+      error: 'Invalid sender email',
+    };
   }
 
-  if(!validateString(message, 5000)) {
+  if (!validateString(message, 5000)) {
     return {
-        error: 'Invalid message',
-    }
+      error: 'Invalid message',
+    };
   }
 
-  await resend.emails.send({
-    from: 'onboarding@resend.dev',
-    to: 'edzanko24@gmail.com',
-    subject: 'Message from contact from',
-    reply_to: senderEmail as string, 
-    text: message as string,
-  });
+  try {
+    await resend.emails.send({
+      from: 'Contact Form <onboarding@resend.dev>',
+      to: 'edzanko24@gmail.com',
+      subject: 'Message from contact from',
+      reply_to: senderEmail as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      })
+    });
+  } catch (error: unknown) {
+    return {
+        error: getErrorMessage(error)
+    };
+  }
 };
+
+
--- components/contact.tsx
+++ components/contact.tsx
@@ -5,7 +5,9 @@ import SectionHeading from './section-heading';
 import { FaPaperPlane } from 'react-icons/fa';
 import { motion } from 'framer-motion';
 import { useSectionInView } from '@/lib/hooks';
-import { sendEmail } from '@/actions/sendEmail';
+import { senderEmail } from '@/actions/sendEmail';
+import { useFormStatus } from 'react-dom';
+import SubmitBtn from './submit-btn';
 
 export default function Contact() {
   const { ref } = useSectionInView('Contact');
@@ -39,7 +41,7 @@ export default function Contact() {
       <form
         className='mt-10 flex flex-col'
         action={async formData => {
-          await sendEmail(formData);
+          await senderEmail(formData);
         }}
       >
         <input
@@ -57,18 +59,8 @@ export default function Contact() {
           required
           maxLength={5000}
         />
-        <button
-          className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none
-        transition-all  focus:scale-110 
-        hover:scale-110 hover:bg-gray-950 active:scale-105'
-          type='submit'
-        >
-          Submit{' '}
-          <FaPaperPlane
-            className='text-xs opacity-70 transition-all
-          group-hover:translate-x-1 group-hover:-translate-y-1'
-          />{' '}
-        </button>
+        <SubmitBtn/>
+
       </form>
     </motion.section>
   );
