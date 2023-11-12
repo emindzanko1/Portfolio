286e0b2fbf369981d4299f5d7d846216083f1c2e
emindzanko1 <edzanko1@etf.unsa.ba>
2023-11-12 21:59:30 +0100

    First version of the portfolio - all devices

--- components/project.tsx
+++ components/project.tsx
@@ -26,11 +26,11 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
       <section
         className='bg-gray-100 max-w-[42rem]
     border border-black/5 rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]
-    even:pl-8 hover:bg-gray-200 transition group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20'
+     hover:bg-gray-200 transition sm:group-even:pl-8 dark:text-white dark:bg-white/10 dark:hover:bg-white/20'
       >
         <div
           className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full
-        group-even:ml-[18rem]'
+        sm:group-even:ml-[18rem]'
         >
           <h3 className='text-2xl font-semibold'>{title}</h3>
           <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
@@ -50,7 +50,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
           src={imageUrl}
           alt='Project I worked on'
           quality={95}
-          className='absolute top-8 -right-40
+          className='absolute hidden sm:block top-8 -right-40
       w-[28.25rem] h-[20rem] rounded-t-lg shadow-2xl
       group-even:-right-[initial] group-even:-left-40
       group-hover:-translate-x-3
