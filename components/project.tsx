50baf3e4435cb535b8248f5c2f72a0f25caab9ef
emindzanko1 <edzanko1@etf.unsa.ba>
2024-03-05 15:41:35 +0100

    update march 2024 2.0

--- components/project.tsx
+++ components/project.tsx
@@ -17,6 +17,13 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
   const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
 
+  function capitalizeExcept(str: string, exceptions: string[]): string {
+    return str
+      .split(' ')
+      .map(word => exceptions.includes(word.toLowerCase()) ? word : word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+
   return (
     <motion.div
       ref={ref}
@@ -32,7 +39,7 @@ export default function Project({ title, description, tags, imageUrl }: ProjectP
           className='pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full
         sm:group-even:ml-[18rem]'
         >
-          <h3 className='text-2xl font-semibold'>{title}</h3>
+          <h3 className='font-semibold capitalize'>{capitalizeExcept(title, ['at'])}</h3>
           <p className='mt-2 leading-relaxed text-gray-700 dark:text-white/70'>{description}</p>
           <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
             {tags.map((tag, index) => (
